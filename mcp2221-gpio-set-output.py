6aa76d776949b5ecbf9d6b7b6602de9e8fae09f8
Maciej Kopeć <maciejpkopec@gmail.com>
2025-06-01 00:02:13 +0200

    Simple GPIO script

---
+++ mcp2221-gpio-set-output.py
@@ -0,0 +1,63 @@
+import argparse
+from time import sleep
+
+import EasyMCP2221
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='mcp2221-gpio-set-output',
+        description=('Set a state on a given GPIO output to a declared value of the '
+                     'first found MCP2221.')
+    )
+    parser.add_argument("-g", "--gpio",
+                        nargs="+",
+                        choices=(0, 1, 2, 3),
+                        required=True)
+    parser.add_argument("-s", "--state",
+                        nargs="+",
+                        choices=(0, 1),
+                        required=True)
+    args = parser.parse_args()
+
+    mcp = EasyMCP2221.Device()
+
+    # chip_settings = mcp.send_cmd([0xb0, 0x00])
+
+    # for i, v in enumerate(chip_settings[:14]):
+    #     print(f"{i:02d}: {v:02X}")
+
+    # print(chip_settings[13]*2)
+
+    # print("=========================================")
+
+    # write_chip_settings = mcp.send_cmd([0xb1, 0x00, *chip_settings[4:13], 125])
+
+    # for i, v in enumerate(write_chip_settings[:14]):
+    #     print(f"{i:02d}: {v:02X}")
+
+    # print("=========================================")
+
+    # chip_settings = mcp.send_cmd([0xb0, 0x00])
+
+    # for i, v in enumerate(chip_settings[:14]):
+    #     print(f"{i:02d}: {v:02X}")
+
+    # print(chip_settings[13]*2)
+
+    if len(args.state) != len(args.gpio):
+        raise ValueError("The number of the given GPIOs needs to be equal to the "
+                         "number of states.")
+
+    gpio_dir_map = {}
+    gpio_state_map = {}
+    for gpio, state in zip(args.gpio, args.state):
+        gpio_dir_map[f"gp{gpio}"] = "GPIO_OUT"
+        gpio_state_map[f"gp{gpio}"] = state
+
+    mcp.set_pin_function(**gpio_dir_map)
+    mcp.GPIO_write(**gpio_state_map)
+
+
+if __name__ == "__main__":
+    main()
